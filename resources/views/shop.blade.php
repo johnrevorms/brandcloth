8c84823c4303863eee77669fced3dc31f967a955
john revo <arcanumzxcvbnmm@gmail.com>
2025-06-23 20:55:12 +0700

    Penyesuaian api

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -24,7 +24,7 @@ axios.get(`${backendUrl}/api/products/`)
       card.className = 'bg-white p-4 rounded-lg shadow-md';
       card.innerHTML = `
         <a href="/produk/${product.id}">
-          <img src="${imageUrl}" alt="${product.name}" class="w-full h-[400px] object-cover rounded-md" />
+          <img src="${imageUrl}" alt="${product.name}" class="w-full h-[500px] object-contain rounded-md " />
           <div class="mt-2">
             <h3 class="text-xl font-semibold">${product.name}</h3>
             <p class="text-gray-600">Rp ${parseInt(product.price).toLocaleString('id-ID')}</p>
